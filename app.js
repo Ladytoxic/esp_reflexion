2a78cfc8c9609174e15591343793850c7ec20b03
Almendra Romina R <Almendraromina1@gmail.com>
2023-05-24 01:36:17 -0300

    inicio de proyecto

---
+++ app.js
@@ -0,0 +1,144 @@
+const $fecha = document.getElementById('fecha');
+setInterval(() => {
+    const fechaActual = new Date();
+    const Fecha = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const Hora = { hour: '2-digit', minute: '2-digit', second: '2-digit' };
+    const fechaLegible = fechaActual.toLocaleDateString('es-ES', Fecha) + ' ' + fechaActual.toLocaleTimeString('es-ES', Hora);
+    $fecha.innerText = fechaLegible;
+}, 1000);
+
+// titulo 
+function Welcome() {
+    const $header = document.getElementById('header');
+    const propText = 'e';
+    $header.innerHTML = `<h1>Bienvenid<span class="prop">${propText}</span>s a Disidir</h1>`;
+    const propElements = $header.querySelectorAll('.prop');
+    let index = 0;
+    function changePropText() {
+        let texts = ['a', '@', 'x', 'o', 'e', '*'];
+        if (index >= texts.length) {
+            index = 0;
+        }
+        for (let i = 0; i < propElements.length; i++) {
+            propElements[i].textContent = texts[index];
+        }
+        index++;
+    }
+    setInterval(changePropText, 1000);
+    return $header;
+};
+
+// navbar 
+
+const menuButton = document.getElementById("menu");
+const menuList = document.querySelector("nav ul");
+
+menuButton.addEventListener("click", function () {
+    menuList.classList.toggle("no-visible");
+    if (menuList.classList.contains("no-visible")) {
+        menuButton.innerHTML = '<i class="bi bi-list"></i>';
+    } else {
+        menuButton.innerHTML = '<i class="bi bi-x-lg"></i>';
+    }
+});
+
+const links = document.querySelectorAll('nav a');
+
+links.forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        const targetId = link.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        
+        if (targetId === "") {
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+        } else if (targetElement) {
+            const targetOffset = targetElement.offsetTop - 90;
+            window.scrollTo({ top: targetOffset, behavior: 'smooth' });
+        }
+        
+        menuList.classList.add("no-visible");
+        menuButton.innerHTML = '<i class="bi bi-list"></i>';
+    });
+});
+
+
+// articles
+
+const container = document.querySelector('section');
+
+// Datos de los artículos
+const articlesData = [
+    {
+        id: 'quienes_somos',
+        title: '¿Quiénes Somos?',
+        content: 'Somos una iniciativa impulsada por Amazonas del Sur, la Secretaría de Extensión de la Universidad de Quilmes y el Centro Cultural La Terraza.'
+    },
+    {
+        id: 'cuando_se_realiza',
+        title: '¿Cuándo se realiza?',
+        content: 'Realizamos 2 encuentros por mes de manera presencial.\nTe invitamos a unirte a nosotres en los segundos y cuartos sábados de cada mes, de 17:00 a 19:00 horas.'
+    },
+    {
+        id: 'donde_se_hace',
+        title: '¿Dónde se hace?',
+        content: 'En el centro cultural "La Terraza"\nAv. Aviación 690 (1° Piso)\nLongchamps (Frente a la estación)'
+    }
+];
+
+// Generar los elementos y agregarlos al contenedor
+articlesData.forEach(articleData => {
+    const article = document.createElement('article');
+    article.id = articleData.id;
+
+    const title = document.createElement('h2');
+    title.textContent = articleData.title;
+
+    const content = document.createElement('p');
+    content.textContent = articleData.content;
+
+    article.appendChild(title);
+    article.appendChild(content);
+    container.appendChild(article);
+});
+
+// formulario
+const $formulario = document.getElementById('ContactWhatsApp');
+
+$formulario.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const TEL = '+5491162287249';
+
+    const nombre = $formulario.nombre.value;
+    const pronombres = $formulario.Pronombres.value;
+    const email = $formulario.email.value;
+    const telefono = $formulario.telefono.value;
+    const mensaje = $formulario.mensaje.value;
+
+    const mensajeInicial = `Hola! Mi nombre es ${nombre}(${pronombres}). Estoy necesitando ponerme en contacto.`;
+
+    const textoCompleto = `${mensajeInicial}\n\n*Mensaje:*\n${mensaje}\n\n*Información de Contacto:*\nEmail: ${email}\nTeléfono: ${telefono}\nSaludos!`;
+
+    console.log(textoCompleto);
+
+    const enlaceWhatsApp = `https://wa.me/${TEL}?text=${encodeURIComponent(textoCompleto)}`;
+    window.open(enlaceWhatsApp, '_blank');
+});
+
+
+const $textarea = document.getElementById("msg");
+
+const initialHeigth = parseInt(getComputedStyle($textarea).getPropertyValue('height'));
+
+$textarea.addEventListener('input', () => {
+    $textarea.style.height = `${initialHeigth}px`;
+    const newHeight = $textarea.scrollHeight + initialHeigth;
+    $textarea.style.height = `${newHeight}px`;
+    console.log(newHeight)
+});
+
+
+
+
+
+
